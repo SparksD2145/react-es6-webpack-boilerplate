995adfac628246b300e0ca2e272469ea7cecfdbd
Thomas Ibarra <sparksd2145@gmail.com>
2016-10-11 10:28:06 -0500

    chore(webpack): update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -45,31 +45,33 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:5000',
-    'webpack/hot/dev-server',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'src'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  resolve: {
-    extensions: ['', '.js']
-  },
-  devtool: 'eval-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'src')
-      }
-    ]
-  }
+    entry: [
+        'webpack-dev-server/client?http://localhost:5000',
+        'webpack/hot/dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'src'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['babel'],
+            include: path.join(__dirname, 'src')
+        }, {
+            test: /\.less$/,
+            loader: 'style!css!less',
+            include: path.join(__dirname, 'src')
+        }]
+    }
 };
